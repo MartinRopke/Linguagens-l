120c2182a88b117946510789afc5ccb81e696ee6
MartinRopke <martin.ropke@gmail.com>
2021-03-18 09:19:05 -0300

    Criação do validador de CPF

---
+++ validadorCPF/src/martin/ropke/Main.java
@@ -0,0 +1,10 @@
+package martin.ropke;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "510.597.688-60";
+        System.out.println("CPF Valido:"+ ValidadorCPF.validarCpf(cpf));
+
+    }
+}
