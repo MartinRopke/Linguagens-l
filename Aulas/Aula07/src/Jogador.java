61459164b432e28e16c36760b4cc8530a4f84b91
MartinRopke <martin.ropke@gmail.com>
2021-04-19 11:22:25 -0300

    testes

--- Aulas/Aula07/src/Jogador.java
+++ Aulas/Aula07/src/Jogador.java
@@ -2,9 +2,10 @@ public class Jogador {
     private String nome;
     private boolean ehHumano;
     private Jogada jogada;
-    
+
     public Jogador(String nome, boolean ehHumano) {
         this.nome = nome;
+
         this.ehHumano = ehHumano;
     }
 
@@ -13,19 +14,19 @@ public class Jogador {
         this.ehHumano = false;
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-    public boolean isEhHumano9(){
+    public boolean isEhHumano9() {
         return ehHumano;
     }
-    
-    public Jogada getJogada(){
+
+    public Jogada getJogada() {
         return jogada;
     }
-    
-    public void setJogada(Jogada jogada){
+
+    public void setJogada(Jogada jogada) {
         this.jogada = jogada;
     }
 }
