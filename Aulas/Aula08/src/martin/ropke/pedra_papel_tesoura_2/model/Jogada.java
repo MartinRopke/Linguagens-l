48df1f1acd7cab821a0cd22bb9677bae0dfe321e
MartinRopke <martin.ropke@gmail.com>
2021-04-26 11:55:11 -0300

    Transformar classe Jogada abstract

--- Aulas/Aula08/src/martin/ropke/pedra_papel_tesoura_2/model/Jogada.java
+++ Aulas/Aula08/src/martin/ropke/pedra_papel_tesoura_2/model/Jogada.java
@@ -2,11 +2,8 @@ package martin.ropke.pedra_papel_tesoura_2.model;
 
 import martin.ropke.pedra_papel_tesoura_2.enumarecao.Resultado;
 
-public class Jogada {
-    private Jogada venco;
-    private Jogada perco;
-    
-    public Resultado verificarResultado(Jogada jogada) {
+public abstract class Jogada {
+    final public Resultado verificarResultado(Jogada jogada) {
         if(verificarSeGanhei(jogada)){
             return Resultado.GANHOU;
         }
@@ -16,12 +13,7 @@ public class Jogada {
         return Resultado.EMPATOU;
     }
 
-    public boolean verificarSePerdi(Jogada jogada){
-        return true;
-    }
-
-    public boolean verificarSeGanhei(Jogada jogada){
-        return true;
-    }
+    public abstract boolean verificarSePerdi(Jogada jogada);
+    public abstract boolean verificarSeGanhei(Jogada jogada);
     
 }
