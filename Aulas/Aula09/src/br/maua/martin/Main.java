7106fa07a56202cca6cd05a383dc6264a906d3f7
MartinRopke <martin.ropke@gmail.com>
2021-05-13 08:15:04 -0300

    dd

---
+++ Aulas/Aula09/src/br/maua/martin/Main.java
@@ -0,0 +1,9 @@
+package br.maua.martin;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("inicio do main");
+        metodo1();
+        
+    }
+}
