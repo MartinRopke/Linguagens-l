787ab97e483fb2ca3a1919d76d880ee98d05f905
MartinRopke <martin.ropke@gmail.com>
2021-05-31 08:58:44 -0300

    Aula14

---
+++ Aulas/Aula14/src/br/maua/maps/TreeMapTestDrive.java
@@ -0,0 +1,34 @@
+package br.maua.maps;
+
+import br.maua.models.Item;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapTestDrive {
+    public static void main(String[] args) {
+        Map<String, Item> itemMap = new TreeMap<>();
+
+        //Adiciona itens no mapa
+        itemMap.put("Item1", new Item("Maca", 1));
+        itemMap.put("Item2", new Item("Pera", 2));
+        itemMap.put("Item3", new Item("Maca", 1));
+        itemMap.put("Item1", new Item("Banana", 3));
+
+        //Passa por todos os elementos
+        itemMap.forEach((chave, valor) -> System.out.println("Chave:"+chave+"\tValor:"+valor) );
+
+        //Imprimi todos os elementos do Map
+        System.out.println(itemMap);
+
+        //Outra forma de recuperar os itens do map
+        Item item = itemMap.get("Item1");
+        System.out.println(item);
+        item = itemMap.get("Goku");
+        System.out.println(item);
+        item = itemMap.getOrDefault("Vegeta", new Item("Ola", 100));
+        System.out.println(item);
+        System.out.println(itemMap);
+    }
+}
